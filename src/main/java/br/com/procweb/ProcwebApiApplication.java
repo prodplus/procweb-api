2044e9691eb436518e4ffc0ff8b9154e59fd3310
Marlon F. Garcia <marlon_garcia@outlook.com>
2020-10-09 09:52:01 -0300

    primeiro commit

---
+++ src/main/java/br/com/procweb/ProcwebApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.procweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcwebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcwebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/procweb/ProcwebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.procweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcwebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
